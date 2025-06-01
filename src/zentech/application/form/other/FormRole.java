9e690865afb4ac2116e557fc9c87239bbbf26f42
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-06-01 14:50:31 +0700

    feat/base-ui

--- src/zentech/application/form/other/FormRole.java
+++ src/zentech/application/form/other/FormRole.java
@@ -1,9 +1,93 @@
 package zentech.application.form.other;
 
+import com.formdev.flatlaf.FlatClientProperties;
+import java.awt.Component;
+import java.text.DecimalFormat;
+import java.util.Random;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
 public class FormRole extends javax.swing.JPanel {
 
     public FormRole() {
         initComponents();
+        initalUI(tblRole);
+        testData(tblRole);
+    }
+
+    private void testData(JTable table) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.addRow(getRandomRow("Beer"));
+        model.addRow(getRandomRow("Shirt"));
+        model.addRow(getRandomRow("Laptop"));
+        model.addRow(getRandomRow("Book"));
+        model.addRow(getRandomRow("Coffee"));
+        model.addRow(getRandomRow("Phone"));
+        model.addRow(getRandomRow("Chair"));
+        model.addRow(getRandomRow("Watch"));
+        model.addRow(getRandomRow("Sunglasses"));
+        model.addRow(getRandomRow("Bag"));
+        model.addRow(getRandomRow("Headphones"));
+        model.addRow(getRandomRow("Camera"));
+        model.addRow(getRandomRow("Necklace"));
+        model.addRow(getRandomRow("Perfume"));
+        model.addRow(getRandomRow("Wallet"));
+        model.addRow(getRandomRow("Jacket"));
+        model.addRow(getRandomRow("Bicycle"));
+        model.addRow(getRandomRow("Game console"));
+        model.addRow(getRandomRow("Tennis racket"));
+    }
+
+    private Object[] getRandomRow(String name) {
+        DecimalFormat df = new DecimalFormat("#,##0.##");
+        return new Object[]{false, name, "$" + df.format(getAmount(9999, 9999999)), "$" + df.format(getAmount(9999, 9999999)), df.format(getAmount(-100, 100))};
+    }
+
+    private double getAmount(int from, int to) {
+        Random ran = new Random();
+        return (ran.nextInt(to - from) + from) * ran.nextDouble();
+    }
+
+    private void initalUI(JTable table) {
+
+        JScrollPane scroll = (JScrollPane) table.getParent().getParent();
+        scroll.setBorder(BorderFactory.createEmptyBorder());
+        scroll.getVerticalScrollBar().putClientProperty(FlatClientProperties.STYLE, ""
+                + "background:$Table.background;"
+                + "track:$Table.background;"
+                + "trackArc:999");
+
+        table.getTableHeader().putClientProperty(FlatClientProperties.STYLE_CLASS, "table_style");
+        table.putClientProperty(FlatClientProperties.STYLE_CLASS, "table_style");
+
+        table.getTableHeader().setDefaultRenderer(getAlignmentCellRender(table.getTableHeader().getDefaultRenderer(), true));
+        table.setDefaultRenderer(Object.class, getAlignmentCellRender(table.getDefaultRenderer(Object.class), false));
+    }
+
+    private TableCellRenderer getAlignmentCellRender(TableCellRenderer oldRender, boolean header) {
+        return new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component com = oldRender.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (com instanceof JLabel) {
+                    JLabel label = (JLabel) com;
+                    if (column == 0 || column == 4) {
+                        label.setHorizontalAlignment(SwingConstants.CENTER); //Căn giữa
+                    } else if (column == 2 || column == 3) {
+                        label.setHorizontalAlignment(SwingConstants.TRAILING); //Căn trái
+                    } else {
+                        label.setHorizontalAlignment(SwingConstants.CENTER); 
+                    }
+                }
+                return com;
+            }
+        };
     }
 
     @SuppressWarnings("unchecked")
@@ -15,10 +99,14 @@ public class FormRole extends javax.swing.JPanel {
         txtSearch = new javax.swing.JTextField();
         btnAdd = new javax.swing.JButton();
         btnUpdate = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tblRole = new javax.swing.JTable();
 
+        crazyPanel1.setFlatLafStyleComponent(new raven.crazypanel.FlatLafStyleComponent(
+            "background:$Table.background;[light]border:0,0,0,0,shade(@background,5%),,20;[dark]border:0,0,0,0,tint(@background,5%),,20",
+            null
+        ));
         crazyPanel1.setMigLayoutConstraints(new raven.crazypanel.MigLayoutConstraints(
             "wrap,fill,insets 15",
             "[fill]",
@@ -28,39 +116,69 @@ public class FormRole extends javax.swing.JPanel {
             }
         ));
 
+        crazyPanel2.setFlatLafStyleComponent(new raven.crazypanel.FlatLafStyleComponent(
+            "background:$Table:background",
+            new String[]{
+                "JTextField.placeholderText=Search;background:@background",
+                "background:lighten(@background,8%);borderWidth:1",
+                "background:lighten(@background,8%);borderWidth:1",
+                "background:lighten(@background,8%);borderWidth:1",
+                ""
+            }
+        ));
         crazyPanel2.setMigLayoutConstraints(new raven.crazypanel.MigLayoutConstraints(
             "",
             "[]push[][]",
             "",
             new String[]{
-                "width 200"
+                "width 400"
             }
         ));
         crazyPanel2.add(txtSearch);
 
-        btnAdd.setText("jButton1");
+        btnAdd.setText("Thêm");
         crazyPanel2.add(btnAdd);
 
-        btnUpdate.setText("jButton2");
+        btnUpdate.setText("Sửa");
         crazyPanel2.add(btnUpdate);
 
-        jButton3.setText("jButton3");
-        crazyPanel2.add(jButton3);
+        btnDelete.setText("Xoá");
+        crazyPanel2.add(btnDelete);
 
         crazyPanel1.add(crazyPanel2);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tblRole.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "", "Demo", "Demo", "Demo", "Demo", "null", "null"
             }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Boolean.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Boolean.class, java.lang.Boolean.class
+            };
+            boolean[] canEdit = new boolean [] {
+                true, false, false, false, false, true, true
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tblRole.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(tblRole);
+        if (tblRole.getColumnModel().getColumnCount() > 0) {
+            tblRole.getColumnModel().getColumn(0).setPreferredWidth(1);
+            tblRole.getColumnModel().getColumn(1).setPreferredWidth(50);
+            tblRole.getColumnModel().getColumn(2).setPreferredWidth(200);
+            tblRole.getColumnModel().getColumn(3).setPreferredWidth(200);
+            tblRole.getColumnModel().getColumn(4).setPreferredWidth(100);
+        }
 
         crazyPanel1.add(jScrollPane1);
 
@@ -69,27 +187,27 @@ public class FormRole extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(210, 210, 210)
-                .addComponent(crazyPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 630, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(242, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(crazyPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1070, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(158, 158, 158)
-                .addComponent(crazyPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(133, Short.MAX_VALUE))
+                .addGap(33, 33, 33)
+                .addComponent(crazyPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 710, Short.MAX_VALUE)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnDelete;
     private javax.swing.JButton btnUpdate;
     private raven.crazypanel.CrazyPanel crazyPanel1;
     private raven.crazypanel.CrazyPanel crazyPanel2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable tblRole;
     private javax.swing.JTextField txtSearch;
     // End of variables declaration//GEN-END:variables
 }
