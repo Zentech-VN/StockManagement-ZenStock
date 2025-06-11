7d28424a890bb53ac7accbe4dd02db673787ede4
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-06-11 20:31:45 +0700

    feat/update-product-entity

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -8,38 +8,42 @@ public class Product {
     private String tenSanPham;
     private String hinhAnh;
     private String chipXuLy;
-    private String dungLuongPin;
-    private String kichThuocManHinh;
-    private String cameraSau;
     private String cameraTruoc;
-    private String thoiGianBaoHanh;
-    private int soLuongTon;
+    private String cameraSau;
     private int thongSo;
     private BigDecimal gia;
     private int trangThai;
-
+    private String dungLuongPin;
+    private String kichThuocManHinh;
+    private String thoiGianBaoHanh;
+    private int soLuongTon;
     //Các bảng liên kết
     private String tenXuatXu;
     private String tenHeDieuHanh;
     private String tenThuongHieu;
     private String tenKhuVuc;
 
+    private int maXuatXu;
+    private int maHeDieuHanh;
+    private int maThuongHieu;
+
     public Product() {
     }
 
-    public Product(int maSanPham, String tenSanPham, String hinhAnh, String chipXuLy, String dungLuongPin, String kichThuocManHinh, String cameraSau, String cameraTruoc, String thoiGianBaoHanh, int thongSo, BigDecimal gia, int trangThai, String tenXuatXu, String tenHeDieuHanh, String tenThuongHieu, String tenKhuVuc) {
+    public Product(int maSanPham, String tenSanPham, String hinhAnh, String chipXuLy, String cameraTruoc, String cameraSau, int thongSo, BigDecimal gia, int trangThai, String dungLuongPin, String kichThuocManHinh, String thoiGianBaoHanh, int soLuongTon, String tenXuatXu, String tenHeDieuHanh, String tenThuongHieu, String tenKhuVuc) {
         this.maSanPham = maSanPham;
         this.tenSanPham = tenSanPham;
         this.hinhAnh = hinhAnh;
         this.chipXuLy = chipXuLy;
-        this.dungLuongPin = dungLuongPin;
-        this.kichThuocManHinh = kichThuocManHinh;
-        this.cameraSau = cameraSau;
         this.cameraTruoc = cameraTruoc;
-        this.thoiGianBaoHanh = thoiGianBaoHanh;
+        this.cameraSau = cameraSau;
         this.thongSo = thongSo;
         this.gia = gia;
         this.trangThai = trangThai;
+        this.dungLuongPin = dungLuongPin;
+        this.kichThuocManHinh = kichThuocManHinh;
+        this.thoiGianBaoHanh = thoiGianBaoHanh;
+        this.soLuongTon = soLuongTon;
         this.tenXuatXu = tenXuatXu;
         this.tenHeDieuHanh = tenHeDieuHanh;
         this.tenThuongHieu = tenThuongHieu;
@@ -77,38 +81,6 @@ public class Product {
     public void setChipXuLy(String chipXuLy) {
         this.chipXuLy = chipXuLy;
     }
-    
-    public int getSoLuongTon() {
-        return soLuongTon;
-    }
-
-    public void setSoLuongTon(int soLuongTon) {
-        this.soLuongTon = soLuongTon;
-    }
-
-    public String getDungLuongPin() {
-        return dungLuongPin;
-    }
-
-    public void setDungLuongPin(String dungLuongPin) {
-        this.dungLuongPin = dungLuongPin;
-    }
-
-    public String getKichThuocManHinh() {
-        return kichThuocManHinh;
-    }
-
-    public void setKichThuocManHinh(String kichThuocManHinh) {
-        this.kichThuocManHinh = kichThuocManHinh;
-    }
-
-    public String getCameraSau() {
-        return cameraSau;
-    }
-
-    public void setCameraSau(String cameraSau) {
-        this.cameraSau = cameraSau;
-    }
 
     public String getCameraTruoc() {
         return cameraTruoc;
@@ -118,12 +90,12 @@ public class Product {
         this.cameraTruoc = cameraTruoc;
     }
 
-    public String getThoiGianBaoHanh() {
-        return thoiGianBaoHanh;
+    public String getCameraSau() {
+        return cameraSau;
     }
 
-    public void setThoiGianBaoHanh(String thoiGianBaoHanh) {
-        this.thoiGianBaoHanh = thoiGianBaoHanh;
+    public void setCameraSau(String cameraSau) {
+        this.cameraSau = cameraSau;
     }
 
     public int getThongSo() {
@@ -146,18 +118,40 @@ public class Product {
         return trangThai;
     }
 
-    public String getTrangThaiText() {
-        if (this.trangThai == 1) {
-            return "Hoạt động";
-        } else if (this.trangThai == 0) {
-            return "Khoá";
-        } else if (this.trangThai == 2) {
-            return "Ngừng bán";
-        }
-        return "Không xác định";
-    }
     public void setTrangThai(int trangThai) {
         this.trangThai = trangThai;
+    }
+
+    public String getDungLuongPin() {
+        return dungLuongPin;
+    }
+
+    public void setDungLuongPin(String dungLuongPin) {
+        this.dungLuongPin = dungLuongPin;
+    }
+
+    public String getKichThuocManHinh() {
+        return kichThuocManHinh;
+    }
+
+    public void setKichThuocManHinh(String kichThuocManHinh) {
+        this.kichThuocManHinh = kichThuocManHinh;
+    }
+
+    public String getThoiGianBaoHanh() {
+        return thoiGianBaoHanh;
+    }
+
+    public void setThoiGianBaoHanh(String thoiGianBaoHanh) {
+        this.thoiGianBaoHanh = thoiGianBaoHanh;
+    }
+
+    public int getSoLuongTon() {
+        return soLuongTon;
+    }
+
+    public void setSoLuongTon(int soLuongTon) {
+        this.soLuongTon = soLuongTon;
     }
 
     public String getTenXuatXu() {
@@ -191,6 +185,41 @@ public class Product {
     public void setTenKhuVuc(String tenKhuVuc) {
         this.tenKhuVuc = tenKhuVuc;
     }
-    
-    
+
+    public String getTrangThaiText() {
+        switch (this.trangThai) {
+            case 0:
+                return "Hoạt động";
+            case 1:
+                return "Khoá";
+            case 2:
+                return "Ngừng bán";
+            default:
+                return "Không xác định";
+        }
+    }
+
+    public int getMaXuatXu() {
+        return maXuatXu;
+    }
+
+    public void setMaXuatXu(int maXuatXu) {
+        this.maXuatXu = maXuatXu;
+    }
+
+    public int getMaHeDieuHanh() {
+        return maHeDieuHanh;
+    }
+
+    public void setMaHeDieuHanh(int maHeDieuHanh) {
+        this.maHeDieuHanh = maHeDieuHanh;
+    }
+
+    public int getMaThuongHieu() {
+        return maThuongHieu;
+    }
+
+    public void setMaThuongHieu(int maThuongHieu) {
+        this.maThuongHieu = maThuongHieu;
+    }
 }
