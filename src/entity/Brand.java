df4d57cdcd8f09775f9b9d529905724e5e24d6e1
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-06-11 20:32:35 +0700

    feat/sub-entity

---
+++ src/entity/Brand.java
@@ -0,0 +1,36 @@
+package entity;
+
+public class Brand {
+
+    private int id;
+    private String ten;
+
+    public Brand() {
+    }
+
+    public Brand(int id, String ten) {
+        this.id = id;
+        this.ten = ten;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    @Override
+    public String toString() {
+        return ten;
+    }
+}
---
+++ src/entity/MadeIn.java
@@ -0,0 +1,35 @@
+package entity;
+
+public class MadeIn {
+    private int id;
+    private String ten;
+
+    public MadeIn() {
+    }
+
+    public MadeIn(int id, String ten) {
+        this.id = id;
+        this.ten = ten;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    @Override
+    public String toString() {
+        return ten;
+    }
+}
