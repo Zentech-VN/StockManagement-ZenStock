7007aef7fc28a0935a3ba2e08a40782b42cf24b9
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-07-25 02:12:04 +0700

    #161 - feat/inital-productOutOfStock-entity

---
+++ src/entity/Chart_ProductOutOfStock.java
@@ -0,0 +1,42 @@
+package entity;
+
+public class Chart_ProductOutOfStock {
+
+    private int maSanPham;
+    private String tenSanPham;
+    private int soLuong;
+
+    public Chart_ProductOutOfStock() {
+    }
+
+    public Chart_ProductOutOfStock(int maSanPham, String tenSanPham, int soLuong) {
+        this.maSanPham = maSanPham;
+        this.tenSanPham = tenSanPham;
+        this.soLuong = soLuong;
+    }
+
+    public int getMaSanPham() {
+        return maSanPham;
+    }
+
+    public void setMaSanPham(int maSanPham) {
+        this.maSanPham = maSanPham;
+    }
+
+    public String getTenSanPham() {
+        return tenSanPham;
+    }
+
+    public void setTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+}
