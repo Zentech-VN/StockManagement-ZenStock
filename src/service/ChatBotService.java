ff2587bf48023b8c95b7bccdc04c477d905dd77d
DucPhamNgoc08 <ducphamngoc39@gmail.com>
2025-08-08 23:47:28 +0700

    update/ChatBotService

--- src/service/ChatBotService.java
+++ src/service/ChatBotService.java
@@ -1,38 +1,20 @@
 package service;
 
-import entity.ChatMessage;
-import entity.ChatSession;
+//import entity.ChatMessage;
+//import entity.ChatSession;
 import java.util.List;
 
 public class ChatBotService {
     private OpenAIService openAIService;
-    private ChatSession currentSession;
 
     public ChatBotService() {
         this.openAIService = new OpenAIService();
-        this.currentSession = new ChatSession();
     }
     
-    public String sendMessage(String userMessage){
-        //Tao tin nhan nguoi dung
-        ChatMessage userChatMessage = new ChatMessage("user", userMessage);
-        currentSession.addMessage(userChatMessage);
-        
-        //gửi tin nhắn đến OpenAI và nhận phản hồi
-        String assistantResponse = openAIService.sendMessage(userMessage, currentSession.getMessages());
-        
-        //tạo tin nhắn phản hồi từ assistant
-        ChatMessage assistantChatMessage = new ChatMessage("assistant", assistantResponse);
-        currentSession.addMessage(assistantChatMessage);
-        
+    public String sendMessage(String userMessage){   
+        String assistantResponse = openAIService.sendMessage(userMessage);
         return assistantResponse;
     }
     
-    public List<ChatMessage> getConversationHistory() {
-        return currentSession.getMessages();
-    }
-    
-    public void newSession() {
-        this.currentSession = new ChatSession();
-    }
+
 }
