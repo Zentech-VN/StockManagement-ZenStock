1dd42408b3e04bf5fd7c69589562e707eb8eee5f
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-08 23:07:04 +0700

    feat/update-entity-for-chart

---
+++ src/entity/Chart_Employee.java
@@ -0,0 +1,23 @@
+package entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@ToString
+public class Chart_Employee {
+
+    int maNhanVien;
+    String hoTen;
+    int tongSoLuong;
+    
+}
---
+++ src/entity/Chart_Supplier.java
@@ -0,0 +1,23 @@
+package entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@ToString
+public class Chart_Supplier {
+
+    int maNhaCungCap;
+    String tenNhaCungCap;
+    int tongSoLuong;
+    
+}
